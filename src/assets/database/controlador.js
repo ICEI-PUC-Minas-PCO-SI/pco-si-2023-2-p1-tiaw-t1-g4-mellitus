6fb851fcee2329d4514981d71bfeb89cbbd52a41
Matheus Freitas <matheus.freitas@aglets.com.br>
2023-12-03 21:01:17 -0300

    feat: manipulação usuarios

--- src/assets/database/controlador.js
+++ src/assets/database/controlador.js
@@ -1,45 +1,90 @@
 const URL = 'http://localhost:3000/'
 
-function getUsuarios(){
-    fetch(`${URL}usuarios`).then(res => res.json()).then(usuarios => {
+function getUsuarios() {
+    fetch(`${URL}usuarios?_sort=id&_order=asc`).then(res => res.json()).then(usuarios => {
         return usuarios;
     })
 }
 
-function getUsuario(idBusca = null, nomeBusca = null){
-
+function getUsuario(idBusca) {
     let response = {
         resultado: false,
         mensagem: "",
         dados: ""
     };
 
-    if(idBusca !== null || nomeBusca !== null){
-        fetch(`${URL}usuarios`).then(res => res.json()).then(usuarios => {
-            for (let i = 0; i < usuarios.length; i++) {
-                let usuario = usuarios[i];
-                if(idBusca === null){
-                    if(usuario.nome == nomeBusca){
-                        response.resultado = true;
-                        response.mensagem = "Usuario encontrado.";
-                        response.dados = usuario;
-                        return response;
-                    }
-                }
-
-                if(nomeBusca === null){
-                    if(usuario.id == usuario.id){
-                        response.resultado = true;
-                        response.mensagem = "Usuario encontrado.";
-                        response.dados = usuario;
-                        return response;
-                    }
-                }
+    return fetch(`${URL}usuarios/${idBusca}`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`O usuario não foi encontrado.`);
             }
+            return res.json();
         })
-    }else{
-        response.mensagem = "Nenhum identificador foi informado.";
-    }
+        .then(usuario => {
+            response.resultado = true;
+            response.mensagem = "Usuario encontrado com sucesso.";
+            response.dados = usuario;
+            return response;
+        })
+        .catch(error => {
+            response.mensagem = error.message;
+            return response;
+        });
+}
 
-    return response;
+function setUsuario(newNome, newLogin, newSenha) {
+
+    let lastId;
+
+    fetch(`${URL}usuarios?_sort=id&_order=desc&_limit=1`).then(res => res.json()).then(usuarios => {
+        console.log(usuarios);
+        lastId = usuarios[0].id + 1;
+        console.log(lastId);
+
+        let newUser = JSON.stringify({
+            id: lastId,
+            nome: newNome,
+            login: newLogin,
+            senha: newSenha
+        });
+
+        console.log(newUser);
+
+        fetch(`${URL}usuarios`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: newUser
+        }).then(res => res.json())
+    })
+
+}
+
+function updateUsuario(idBusca, updateNome, updateLogin, updateSenha) {
+
+    let updateUser = JSON.stringify({
+        id: idBusca,
+        nome: updateNome,
+        login: updateLogin,
+        senha: updateSenha
+    });
+
+    fetch(`${URL}usuarios/${idBusca}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: updateUser
+    }).then(res => res.json())
+}
+
+function deletaUsuario(idBusca) {
+
+    fetch(`${URL}usuarios/${idBusca}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).then(res => res.json())
 }
